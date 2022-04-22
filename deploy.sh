a5f4c4fb0b7111aaf701d872cd0ea0c25573efee
Camilo Raitz da Silva <craitzus@gmail.com>
2022-04-22 18:27:26 -0300

    initial repo

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+function_name="aws-node-ghactions"
+alias_name="production"
+
+result=$(sls deploy)
+echo "${result}"
+
+latest_version=$(aws lambda list-versions-by-function --function-name ${function_name} | grep "FunctionArn" | tail -1 | cut -d':' -f 9 | cut -d'"' -f 1)
+echo "Latest version: ${latest_version}"
+
+result=$(aws lambda update-alias --function-name ${function_name} --name ${alias_name} --function-version ${latest_version})
+echo "${result}"
