2842791e91dba54cd81e8b1e5393928b6190d90e
Camilo Raitz da Silva <craitzus@gmail.com>
2022-04-22 19:19:43 -0300

    initial repo

--- .github/deploy.dev.sh
+++ .github/deploy.dev.sh
@@ -10,5 +10,5 @@ echo "${result}"
 latest_version=$(aws lambda list-versions-by-function --function-name ${function_name} | grep "FunctionArn" | tail -1 | cut -d':' -f 9 | cut -d'"' -f 1)
 echo "Latest version: ${latest_version}"
 
-result=$(aws lambda update-alias --function-name ${function_name} --name $1 --function-version ${latest_version})
+result=$(aws lambda update-alias --function-name ${function_name} --name develop --function-version ${latest_version})
 echo "${result}"
---
+++ .github/deploy.main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+function_name="aws-node-ghactions"
+
+result=$(sls deploy)
+echo "${result}"
+
+latest_version=$(aws lambda list-versions-by-function --function-name ${function_name} | grep "FunctionArn" | tail -1 | cut -d':' -f 9 | cut -d'"' -f 1)
+echo "Latest version: ${latest_version}"
+
+result=$(aws lambda update-alias --function-name ${function_name} --name production --function-version ${latest_version})
+echo "${result}"
